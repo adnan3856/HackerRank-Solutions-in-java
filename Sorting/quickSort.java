28267efd194ba70f208bb057bb0d1fc0273558e8
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-10-23 11:07:18 +0530

    Create quickSort.java
    https://www.hackerrank.com/challenges/quicksort1/problem?isFullScreen=true

---
+++ Sorting/quickSort.java
@@ -0,0 +1,18 @@
+    static int[] quickSort(int[] arr) {
+        int pivot=arr[0];
+        int n=arr.length;
+    for(int i=1;i<n;i++){
+        if(pivot>arr[i]){
+            for(int j=i;j>0;j--){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+        }
+    }
+}
+return arr;
+
+    }
+    
+    //hackerrank problem
+    //https://www.hackerrank.com/challenges/quicksort1/problem
